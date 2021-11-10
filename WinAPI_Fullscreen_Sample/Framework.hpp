f24aa6a9e492de8f26186ed95c18a2d89170c84f
dotBlueShoes <smatthewbloodson@gmail.com>
2021-11-10 20:20:13 +0100

    Setting up the structure
    Basic auto generated windows app with no changes to code itself.

---
+++ WinAPI_Fullscreen_Sample/Framework.hpp
@@ -0,0 +1,19 @@
+﻿// Code in this file represents the includes shared among all other code files in this project.
+
+#pragma once
+
+#include "settings/targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
+#include <windows.h>					// Pliki nagłówkowe systemu Windows
+
+// Pliki nagłówkowe środowiska uruchomieniowego języka C
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+// Resource - Files that are coresponding for menus, buttons and other.
+
+#include "resource/resource.h"
