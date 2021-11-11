a5ec4ba245334e6d859c9ca3ae4a4e38ea7b943d
dotBlueShoes <smatthewbloodson@gmail.com>
2021-11-11 12:20:49 +0100

    Setup x86 directory path

--- Cpp_WinAPI_Fullscreen_Sample/Framework.hpp
+++ Cpp_WinAPI_Fullscreen_Sample/Framework.hpp
@@ -4,13 +4,13 @@
 
 #pragma once
 
-
-#include "settings/targetver.h"					// WinAPI version control - GUI look.
+#include "settings/targetver.h"			// WinAPI version control - GUI look.
 
 #define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
 #include <windows.h>					// Pliki nagłówkowe systemu Windows
 
 // Pliki nagłówkowe środowiska uruchomieniowego języka C
+
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
